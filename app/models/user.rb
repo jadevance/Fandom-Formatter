6e1742dfa8508839ea75cc912de5b4a80b3e2759
Jade Vance <jadenicolevance@gmail.com>
2016-06-01 12:24:32 -0700

    added columns to user model to handle google oauth2 info and tokens. updated user model to reflect changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,27 @@
 class User < ActiveRecord::Base
   validates :provider, presence: true
   validates :uid, presence: true
-  validate :provider_must_be_google
+  validate  :provider_must_be_google
 
   def provider_must_be_google
-    if provider != "google"
-      errors.add(:provider, "provider must be google")
+    if provider != "google_oauth2"
+      errors.add(:provider, "provider must be google_oauth2")
     end
   end
 
 
   def self.find_or_create_from_omniauth(auth_hash)
-    user = self.find_by(uid: auth_hash['info']['id'], provider: auth_hash['provider'])
+    user = self.find_by(uid: auth_hash['info']['uid'], provider: auth_hash['provider'])
     if !user.nil?
       return user
     else
-      user              = User.new
-      user.uid          = auth_hash['info']['id']
-      user.provider     = auth_hash['provider']
-      # user.display_name = auth_hash["info"]["display_name"]
-      # user.photo_url    = auth_hash["info"]['images'][0]['url']
-
+        user = User.new 
+        user.provider = auth_hash.provider
+        user.uid = auth_hash.uid
+        user.name = auth_hash.info.name
+        user.oauth_token = auth_hash.credentials.token
+        user.oauth_expires_at = Time.at(auth_hash.credentials.expires_at)
+        user.save!
       if user.save
         return user
       else
--- db/migrate/20160529201543_create_users.rb
+++ db/migrate/20160529201543_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :name, null: false
+      t.string :oauth_token, null: false
+      t.datetime :oauth_expires_at, null: false 
       t.timestamps null: false
     end
   end
