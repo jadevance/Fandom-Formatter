7d2596e076bc908be5fc951cf266622bada6db0e
Jade Vance <jadenicolevance@gmail.com>
2016-06-03 20:36:28 -0700

    updated omniauth to handle a specific error (invalid IAT) as well as added a route for about

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true,
     {client_options: 
       {ssl: 
         {ca_file: Rails.root.join("cacert.pem").to_s}
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  get '/home' => 'home#index'
+  get '/about', to: 'home#about', as: 'about'
   post '/home' => 'home#create', as: :files   
 
   resources :sessions, only: [:create, :destroy]
-  resources :home, only: [:index]
 
   root to: "home#index"
 end
