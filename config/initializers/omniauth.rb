7dfe50f1c0d147aced96a7c36b18df9ec3a34e8f
Jade Vance <jadenicolevance@gmail.com>
2016-06-04 09:54:12 -0700

    removed skip jwt to see if it clears the heroku rejection

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true,
-    {client_options: 
-      {ssl: 
-        {ca_file: Rails.root.join("cacert.pem").to_s}
-      }
-    }
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
+    {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
