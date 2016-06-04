73b02234d7d8ab917e31f3fe72173c094e58312f
Jade Vance <jadenicolevance@gmail.com>
2016-06-04 09:46:11 -0700

    Wrote a test helper (but commented out vcr because it is not useful yet) and wrote some user tests for omniauth

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @known = { "provider" => "google_oauth2", "info" =>  { "id" => "known_user", "display_name" => "known user" , "images" => ["url" => "http://i.imgur.com/2D35e.jpg"]}}
+    @unknown = { "provider" => "google_oauth2", "info" => { "id" => "unknown_user", "display_name" => "unknown user" , "images" => ["url" => "http://i.imgur.com/2D35e.jpg"]} }
+    @unknown_with_uid = OmniAuth.config.mock_auth[:spotify_uid]
+  end
+  
+  test "creates new user with oauth google hash of unknown user" do
+    assert_difference 'User.count', 1 do
+      @user = User.find_or_create_from_omniauth @unknown
+    end
+  end
+
+  test "finds an existing user given an oauth google hash" do
+    assert_equal users(:known_user), User.find_or_create_from_omniauth(@known)
+  end
+
+  test "uses oauth data to set data for new users" do
+   user = User.find_or_create_from_omniauth @unknown
+
+   assert_equal @unknown['provider'], user.provider
+   assert_equal @unknown['info']['id'], user.uid
+   assert_equal @unknown['info']['display_name'], user.display_name
+  end
+
+  test "validation: provider must be google" do
+    user = User.new
+    user.provider = "Googly Eyes"
+    assert_not user.valid?
+    assert user.errors.keys.include?(:provider), "provider is not in the errors hash"
+  end
+
+  test "validation: provider must be present" do
+    user = User.new
+    assert_not user.valid?
+    assert user.errors.keys.include?(:provider), "provider is not the errors hash"
+  end
+
+  test "validation: uid must be present" do
+    user = User.new
+    assert_not user.valid?
+    assert user.errors.keys.include?(:uid), "uid is not in the errors hash"
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,29 @@
+require 'simplecov'
+SimpleCov.start 'rails'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/spec'
+require "minispec-metadata"
+# require 'vcr'
+# require 'minitest-vcr'
+require 'webmock/minitest'
+require "minitest/reporters"
+
+# VCR.configure do |c|
+#   c.cassette_library_dir = 'test/cassettes'
+#   c.hook_into :webmock
+# end
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+MinitestVcr::Spec.configure!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def setup
+   OmniAuth.config.test_mode = true
+   
+   OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({:provider => 'google_oauth2', :uid => '118416535553028371933', info: { name: 'Jade Vance'}})
+  end
 end
