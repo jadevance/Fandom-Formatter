d96e270dc8b47418d2deb17a51e0080cdd45c803
Jade Vance <jadenicolevance@gmail.com>
2016-06-03 18:02:26 -0700

    Added in routes and logic to handle markdown vs markup

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+  get '/home' => 'home#index'
+  post '/home' => 'home#create', as: :files   
 
   resources :sessions, only: [:create, :destroy]
-  resource :home, only: [:index]
+  resources :home, only: [:index]
 
   root to: "home#index"
 end
